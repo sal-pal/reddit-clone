3f44d5e3155913fdfc41bdf4604727bb258a1444
Sal P <salvatpalvat@gmail.com>
2017-06-12 19:21:28 -0400

    Created post factory

--- tests/factory.js
+++ tests/factory.js
@@ -1,24 +1,21 @@
 const faker = require('faker')
-const factory = require('factory-girl').factory
 const ObjectId = require('mongoose').Types.ObjectId
-const Comment = require('../src/models/Comment.js')
-const Post = require('../src/models/Post.js')
 
 
 
-factory.define('Comment', Comment, {
-    comment: () => faker.lorem.sentences(),
-    author: () => ObjectId(),
-    parent: ObjectId()
-})
-
-
-factory.define('Post', Post, {
-    title: () => faker.lorem.words(),
-    body: faker.lorem.sentences(),
-    author: () => ObjectId()
-})
-
-
-
-module.exports = factory
+//Post factory
+module.exports = (quantity) => {
+    var parent = {}
+    var num = 0
+    
+    while (num < quantity) {
+        parent[num] = {
+            title: faker.lorem.words(),
+            body: faker.lorem.sentences(),
+            author: ObjectId()
+        }
+        num += 1
+    }
+    
+    return parent
+}
