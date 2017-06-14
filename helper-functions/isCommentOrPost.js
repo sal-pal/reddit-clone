01321ab5cb4c1695cdc6555b544ecb3acafb6469
Sal P <salvatpalvat@gmail.com>
2017-06-14 19:52:52 -0400

    Created function that checks if an object is a post or comment

---
+++ helper-functions/isCommentOrPost.js
@@ -0,0 +1,69 @@
+/**
+        Determines if an object adheres to either the comment or post schema
+        
+            -Output: A boolean 
+            -Input:
+                a) modelName: (string) represents which schema you wish to test against
+                b) inputObj: the object you wish to test
+                
+            Error Handling:
+                -Throws an error if modelName not assigned either of the strings 'comment' or 'post'
+**/
+
+
+
+const isValidObjectId = require('mongoose').Types.ObjectId.isValid
+
+
+
+
+module.exports = (modelName, inputObj) => {
+    
+    if (typeof modelName !== 'string') {
+        throw new TypeError('Need to pass string for modelName parameter')
+    }
+    
+    if (modelName === 'post') {
+        
+        const inputHasTitle = inputObj.hasOwnProperty('title')
+        const inputHasBody = inputObj.hasOwnProperty('body')
+        const inputHasAuthor = inputObj.hasOwnProperty('author')
+        
+        if (inputHasTitle && inputHasBody && inputHasAuthor) {
+            
+            const titleIsString = (typeof inputObj.title === 'string')
+            const bodyIsString = (typeof inputObj.body === 'string')
+            const authorIsObjectId = isValidObjectId (inputObj.author)
+            
+            if (titleIsString && bodyIsString && authorIsObjectId) {
+                return true
+            }
+            else {return false}
+        }
+        
+        else {return false}
+    }
+    
+    else if (modelName === 'comment') {
+        
+        const inputHasAuthor = inputObj.hasOwnProperty('author')
+        const inputHasParent = inputObj.hasOwnProperty('parent')
+        const inputHasComment = inputObj.hasOwnProperty('comment')
+        
+        if (inputHasAuthor && inputHasParent && inputHasComment) {
+            
+            const authorIsObjectId = isValidObjectId (inputObj.author)
+            const parentIsObjectId = isValidObjectId (inputObj.parent)
+            const commentIsString = (typeof inputObj.comment === 'string')
+            
+            if (authorIsObjectId && parentIsObjectId && commentIsString) {
+                return true
+            }
+            else {return false}
+        }
+        
+        else {return false}    
+    }
+    
+    else {throw new Error("Need to pass either 'comment' or 'post' for modelName")}
+}
