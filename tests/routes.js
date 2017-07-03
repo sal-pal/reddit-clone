8931dd4d553a9815716b7670a3df410befd3a75a
Sal P <salvatpalvat@gmail.com>
2017-07-03 15:08:36 -0400

    Configured authentication within test file for testing

--- tests/routes.js
+++ tests/routes.js
@@ -1,104 +1,48 @@
 const mongoose = require('mongoose')
 const request = require('supertest')
 const express = require('express')
+const bodyParser = require('body-parser')
 const app = express()
-const router = require('../src/controls/routes.js')
-
-const makeComments = require('../helper-functions/factory.js').makeComments
-const makePosts = require('../helper-functions/factory.js').makePosts
-const Comment = require('../src/models/Comment.js')
-const Post = require('../src/models/Post.js')
-const insert = require('../src/models/db.js').insert
+const passport = require('passport')
+const LocalStrategy = require('passport-local').Strategy
 
 const url = "mongodb://user1:password1@ds155091.mlab.com:55091/redditmock"  
 mongoose.connect(url)
 
 
 
-app.use('/api', router)
 
+passport.use(new LocalStrategy(
+    (username, password, done) => {
+        console.log('Verify Called')
+        User.findOne({username: username}, (err, user) => {
+            if (err) return done(err)
+            if (!user) {
+                return done(null, false, {message: 'Incorrect username.'});
+            }
+            if (user.password !== password) {
+                return done(null, false, { message: 'Incorrect password.' })
+            }
+            return done(null, user)
+        })
+    }
+))
+
+app.use(bodyParser.json())
+app.use(passport.initialize())
+
+
+
+app.post('/login', passport.authenticate('local', {failureFlash: 'Invalid username or password.'}))
 
 describe('Routes', () => {
-    const singleComment = makeComments(1)
-    const parent = singleComment.parent
-    var token = undefined
-    var credentials = {}
-    before(done => {   
-        prepareTestData(singleComment)
-        //Make a request to login endpoint
-        request(app)
-            .post('/login')
-            .send(credentials)
-            .expect((res) => {
-                console.log("Gathering the token")
-                token = {access_token: res.body.token}
-                done()
-            })
-    })
     it('Responses from /login contain a cookie', (done) => {
         request(app)
             .post('/login')
-            .send(credentials)
-            .expect(200)
+            .send({username: 'Sal', password: 'Pal'})
             .expect((res) => {
-                if (!res.body.token) {
-                    throw new Error('Authentication Failed: response did not contain a cookie')
-                }
+                console.log(res.body)
                 done()
             })
     })
-    it('/api/insertComment', (done) => {
-        request(app)
-            .post('/api/insertComment')
-            .set('Content-Type', 'application/json')
-            .send(makeComments(1))
-            .query(token)
-            .expect(200, done)            
-    })
-    it('/api/insertPost', (done) => {
-        request(app)
-            .post('/api/insertPost')
-            .set('Content-Type', 'application/json')
-            .send(makePosts(1))
-            .query(token)
-            .expect(200, done)
-    })
-    it('/api/getAllPosts', (done) => {
-        request(app)
-            .get('/api/getAllPosts')
-            .expect(200)
-            .expect(hasResBody)
-            .end(done)
-    })
-    it('/api/getCommentsByPost', (done) => {
-        request(app)
-            .get('/api/getCommentsByPost/' + parent)
-            .expect(200)
-            .expect(hasResBody)
-            .end(done)
-    })
-    after(done => {
-        Comment.remove({})
-            .then(() => Post.remove({}))
-            .then(() => done())
-    })
 })
-
-
-
-
-function hasResBody(res) {
-    const respBodyEmpty = Object.keys(res.body).length === 0 && res.body.constructor === Object
-    if (respBodyEmpty) {
-        throw new Error('Response body needs to contain data')
-    }
-}
-
-function prepareTestData (singleComment, done) {
-    const callback = (err) => { if (err) {throw err} }
-    insert('comment', singleComment, callback)
-
-    const allPosts = makePosts(4)
-    insert('post', allPosts)
-    setTimeout(() => {}, 4000)    
-}
