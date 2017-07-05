97c348fa98dda0151d5ac765031de22806b1b638
Sal P <salvatpalvat@gmail.com>
2017-07-05 13:00:42 -0400

    Added form data to request in test file

--- tests/routes.js
+++ tests/routes.js
@@ -1,4 +1,3 @@
-const mongoose = require('mongoose')
 const request = require('supertest')
 const express = require('express')
 const bodyParser = require('body-parser')
@@ -6,31 +5,16 @@ const app = express()
 const passport = require('passport')
 const LocalStrategy = require('passport-local').Strategy
 
-const url = "mongodb://user1:password1@ds155091.mlab.com:55091/redditmock"  
-mongoose.connect(url)
 
 
 
 
 passport.use(new LocalStrategy(
-    (username, password, done) => {
-        console.log('Verify Called')
-        User.findOne({username: username}, (err, user) => {
-            if (err) return done(err)
-            if (!user) {
-                return done(null, false, {message: 'Incorrect username.'});
-            }
-            if (user.password !== password) {
-                return done(null, false, { message: 'Incorrect password.' })
-            }
-            return done(null, user)
-        })
-    }
+    (username, password, done) => console.log('Verify Called')
 ))
 
 app.use(bodyParser.json())
 app.use(passport.initialize())
-
 
 
 app.post('/login', passport.authenticate('local', {failureFlash: 'Invalid username or password.'}))
@@ -39,10 +23,9 @@ describe('Routes', () => {
     it('Responses from /login contain a cookie', (done) => {
         request(app)
             .post('/login')
-            .send({username: 'Sal', password: 'Pal'})
-            .expect((res) => {
-                console.log(res.body)
-                done()
-            })
+            .type('form')
+            .send({username: 'srpalo'})
+            .send({password: 'secretpassword'})
+        done()
     })
 })
