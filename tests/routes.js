fc77f362995b0efbb50cd806887f782c21d91cf0
Sal P <salvatpalvat@gmail.com>
2017-07-05 23:39:10 -0400

    Can now retrieve user's credentials from the database inside verify callback (in test file)

--- tests/routes.js
+++ tests/routes.js
@@ -4,17 +4,29 @@ const bodyParser = require('body-parser')
 const app = express()
 const passport = require('passport')
 const LocalStrategy = require('passport-local').Strategy
+const User = require('../src/models/User.js')
 
-
+const mongoose = require('mongoose')
+const url = "mongodb://user1:password1@ds155091.mlab.com:55091/redditmock"  
+mongoose.connect(url)
 
 
 
 passport.use(new LocalStrategy(
     (username, password, done) => {
-        console.log('Verify Called')
-        done()
+        User.findOne({username: username}, (err, user) => {
+            if (err) return done(err)
+            if (!user) {
+                return done(null, false, {message: 'Incorrect username.'});
+            }
+            if (user.password !== password) {
+                return done(null, false, { message: 'Incorrect password.' })
+            }
+            return done(null, user)
+        })
     }
 ))
+
 
 app.use(bodyParser())
 app.use(passport.initialize())
@@ -22,17 +34,21 @@ app.use(passport.session())
 
 
 app.post('/login', passport.authenticate('local'), (req, res) => {
-    console.log('Login route hit')
+  console.log('Login route hit')
 })
 
 const server = app.listen(3000)
 
 describe('Routes', () => {
-    it('Responses from /login contain a cookie', () => {
-        request(server)
-            .post('/login')
-            .type('form')
-            .send({username: 'srpalo'})
-            .send({password: 'secretpassword'})
-    })
+  it('Responses from /login contain a cookie', (done) => {
+    request(server)
+      .post('/login')
+      .type('form')
+      .send({username: 'srpalo'})
+      .send({password: 'secretpassword'})
+      .then((res) => {
+        console.log(res.status)
+        done()
+      })
+  })
 })
