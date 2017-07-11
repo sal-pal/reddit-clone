77a11f04d674ec9ab13bce570acc7911e510ad8e
Sal P <salvatpalvat@gmail.com>
2017-07-11 14:38:48 -0400

    Added configuration for image loader

---
+++ src/webpack.config.js
@@ -0,0 +1,29 @@
+const webpack = require('webpack')
+
+module.exports = {
+  context: __dirname,
+  entry: "./App.js",
+
+  output: {
+    filename: "app.js",
+    path: __dirname + '/assets'
+  },
+  module: {
+    loaders: [
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          loader: 'babel-loader',
+          query: {
+            presets: ['react', 'es2015']
+          }
+        },
+        {
+          test: /\.(jpe?g|png|gif|svg)$/i,
+          loaders: [
+            'file?hash=sha512&digest=hex&name=[hash].[ext]',
+            'image-webpack?bypassOnDebug&optimizationLevel=7&interlaced=false'
+          ]
+        }]
+  }
+}
