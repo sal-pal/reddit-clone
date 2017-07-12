ed6072f509712157a8fd2d39a4a68f4d38625232
Sal P <salvatpalvat@gmail.com>
2017-07-12 18:20:51 -0400

    Began debugging our protected routes

--- tests/routes.js
+++ tests/routes.js
@@ -8,6 +8,12 @@ const User = require('../src/models/User.js')
 const expressSession = require('express-session')
 const router = require('../src/controls/routes.js')
 
+const Comment = require('../src/models/Comment.js')
+const Post = require('../src/models/Post.js')
+const insert = require('../src/models/db.js').insert
+const makeComments = require('../helper-functions/factory.js').makeComments
+const makePosts = require('../helper-functions/factory.js').makePosts
+
 const mongoose = require('mongoose')
 const url = "mongodb://user1:password1@ds155091.mlab.com:55091/redditmock"  
 mongoose.connect(url)
@@ -28,9 +34,25 @@ const server = app.listen(3000)
 
 
 describe('Routes', () => {
+    const singleComment = makeComments(1)
+    const parent = singleComment.parent
+    var credentials = {}
+    before(done => {   
+        prepareTestData(singleComment)
+        //Make a request to login endpoint
+        request(server)
+            .post('/login')
+            .type('form')
+            .send({username: 'srpalo'})
+            .send({password: 'secretpassword'})
+            .then((res) => {
+                cookie = res.header['set-cookie'][0]
+                done()
+            })
+    })     
     it('Responses from /login contain a cookie', (done) => {
         request(server)
-            .post('/api/login')
+            .post('/login')
             .type('form')
             .send({username: 'srpalo'})
             .send({password: 'secretpassword'})
@@ -41,7 +63,39 @@ describe('Routes', () => {
                     throw new Error('Authentication Failed: response did not contain a cookie')
                 }
                 done()
-            })
-
+            })   
     })
+    it('/api/insertComment', (done) => {
+        request(server)
+            .post('/insertComment')
+            .set('Content-Type', 'application/json')
+            .send(makeComments(1))
+            .query(cookie)
+            .expect(200, done)            
+    })
+    it('/api/insertPost', (done) => {
+        request(server)
+            .post('/insertPost')
+            .set('Content-Type', 'application/json')
+            .send(makePosts(1))
+            .query(cookie)
+            .expect(200, done)
+    })
+    after(done => {
+        Comment.remove({})
+            .then(() => Post.remove({}))
+            .then(() => done())
+    })    
 })
+
+
+
+
+function prepareTestData (singleComment, done) {
+    const callback = (err) => { if (err) {throw err} }
+    insert('comment', singleComment, callback)
+
+    const allPosts = makePosts(4)
+    insert('post', allPosts)
+    setTimeout(() => {}, 4000)    
+}
