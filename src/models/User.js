b21b191708234dc45f3f13843c6fd08c7b7b61f8
Sal P <salvatpalvat@gmail.com>
2017-07-21 17:52:31 -0400

    Created the implentation used in the tutorial recommended by David.

--- src/models/User.js
+++ src/models/User.js
@@ -1,15 +1,21 @@
 const mongoose = require('mongoose')
+const bcrypt = require('bcrypt-nodejs')
 
 
-const schema = new mongoose.Schema({
-    username: {
-        type: String,
-        required: true
-    },
-    password: {
-        type: String,
-        required: true
-    }
+const userSchema = new mongoose.Schema({
+    email: String,
+    password: String
 }) 
 
-module.exports = mongoose.model('User', schema)
+
+userSchema.methods.generateHash = function(password) {
+    return bcrypt.hashSync(password, bcrypt.genSaltSync(8), null)
+};
+
+// checking if password is valid
+userSchema.methods.validPassword = function(password) {
+    return bcrypt.compareSync(password, this.local.password)
+};
+
+// create the model for users and expose it to our app
+module.exports = mongoose.model('User', userSchema)
