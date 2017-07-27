fee382e7ac1c9d705627771d6153682598363c13
Sal P <salvatpalvat@gmail.com>
2017-07-27 19:14:03 -0400

    About to copy and paste another component in Post.js

--- src/frontend/Post.js
+++ src/frontend/Post.js
@@ -17,13 +17,17 @@
 import React, { Component } from 'react'
 
 class Post extends Component {
-  render() {
-    return (
-      <div className="Post">
-        <h1>Hello World!</h1>
-      </div>
-    );
-  }
+    constructor(props) {
+        super(props)
+    }  
+    
+    render() {
+        return (
+          <div className="Post">
+            <h1>Hello World!</h1>
+          </div>
+        );
+    }
 }
 
 module.exports = Post
