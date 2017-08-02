bcecf82aaac6e494c2a75bdfda5c02ffc2015f12
Sal P <salvatpalvat@gmail.com>
2017-08-02 14:15:10 -0400

    Locally deployed the api

--- server.js
+++ server.js
@@ -1,6 +1,14 @@
 const path = require("path")
 const express = require("express")
 const app = express()
+const mongoose = require('mongoose')
+const router = require('./src/backend/controls/routes.js')
+
+const url = "mongodb://user1:password1@ds155091.mlab.com:55091/redditmock"  
+mongoose.connect(url)
+
+
+app.use('/api', router)
 
 app.use('/', express.static(path.join(__dirname, '/src/frontend/assets')))
 
