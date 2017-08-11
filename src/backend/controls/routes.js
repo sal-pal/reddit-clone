43eb6a24604a5cf13c39b0c9b9c21b3abb33042a
Sal P <salvatpalvat@gmail.com>
2017-08-11 18:56:15 -0400

    Finished with the logging feature

--- src/backend/controls/routes.js
+++ src/backend/controls/routes.js
@@ -3,17 +3,18 @@ const router = express.Router()
 const db = require('../../../src/backend/models/db.js')
 const passport = require('./authentication.js')
 
-const logErr = require('../../../helper-functions/logErr.js')
 const expressSession = require('express-session')
 const verifyAuth = require('../../../helper-functions/verifyAuth.js')
 const bodyParser = require('body-parser')
+const winston = require('winston')
+
+
+//Configuring logger
+winston.add(winston.transports.File, { filename: 'log.txt' });
+winston.level = 'error';
 
 
 
-
-//Used to protect routes against un-authenticated requests
-router.use('/insertComment', verifyAuth)
-router.use('/insertPost', verifyAuth)
 
 
 
@@ -21,7 +22,7 @@ router.post('/login',  (req, res, next) => {
     passport.authenticate('local-login', (err, user, info) => {
         if (err) {
             const logMsg = JSON.stringify({routeName: 'login', errorName: err.name, errMsg: err.message})
-            logErr(logMsg)            
+            winston.log('error', logMsg)          
             return res.sendStatus(500)
         }  
         if (!user) return res.sendStatus(400)
@@ -29,7 +30,7 @@ router.post('/login',  (req, res, next) => {
         req.logIn(user, function(err) {
             if (err) {
                 const logMsg = JSON.stringify({routeName: 'login', errorName: err.name, errMsg: err.message})
-                logErr(logMsg)      
+                winston.log('error', logMsg)      
                 return res.sendStatus(500)
             } 
             return res.end()
@@ -41,7 +42,7 @@ router.post('/signup', (req, res, next) => {
     passport.authenticate('local-signup', (err, user, info) => {
         if (err) {
             const logMsg = JSON.stringify({routeName: 'signup', errorName: err.name, errMsg: err.message})
-            logErr(logMsg) 
+            winston.log('error', logMsg)  
             return res.sendStatus(500)
         }
         if (!user) return res.sendStatus(400)
@@ -49,7 +50,7 @@ router.post('/signup', (req, res, next) => {
         req.logIn(user, function(err) {
             if (err) {
                 const logMsg = JSON.stringify({routeName: 'signup', errorName: err.name, errMsg: err.message})
-                logErr(logMsg)
+                winston.log('error', logMsg) 
                 return res.sendStatus(500)
             }
             return res.end()
@@ -62,7 +63,7 @@ router.post('/insertComment', (req, res) => {
         if (!err) return res.sendStatus(200)
         res.sendStatus(500)
         const logMsg = JSON.stringify({routeName: 'insertComment', errorName: err.name, errMsg: err.message})
-        logErr(logMsg)        
+        winston.log('error', logMsg)         
     })
 })
 
@@ -71,7 +72,7 @@ router.post('/insertPost', (req, res) => {
         if (!err) return res.sendStatus(200)
         res.sendStatus(500)
         const logMsg = JSON.stringify({routeName: 'insertPost', errorName: err.name, errMsg: err.message})
-        logErr(logMsg)
+        winston.log('error', logMsg) 
     })
 })
 
@@ -80,7 +81,7 @@ router.get('/getAllPosts', (req, res) => {
         if (!err) return res.json(result)
         res.sendStatus(500)
         const logMsg = JSON.stringify({routeName: 'getAllPosts', errorName: err.name, errMsg: err.message})
-        logErr(logMsg)
+        winston.log('error', logMsg) 
     })
 })
 
@@ -91,7 +92,7 @@ router.get('/getCommentsByPost/:parent', (req, res) => {
         if (err.message === 'No comments found') return res.sendStatus(400)
         res.sendStatus(500)
         const logMsg = JSON.stringify({routeName: 'getCommentsByPost', errorName: err.name, errMsg: err.message})
-        logErr(logMsg)
+        winston.log('error', logMsg) 
     })
 })
 
